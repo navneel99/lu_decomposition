b40bdeab3d4ff3c2e9ca65b02e17ed22afac8d50
Navneel <navneel99@gmail.com>
2020-01-20 10:44:41 +0530

    Basic pthread implementation started

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 compile:
 	g++ -lpthread code.cpp 
+
+pthread:
+	g++ -o p.out  -fpermissive test.cpp -lpthread
--- test.cpp
+++ test.cpp
@@ -2,12 +2,18 @@
 #include <pthread.h>
 #include <bits/stdc++.h>
 
-#define NUM_THREADS 1
-
 using namespace std;
 
+typedef vector<double> ROW;
+typedef vector<ROW> MATRIX;
 
-void print_vector(vector<vector<int> > v){
+#define NUM_THREADS 2
+
+
+MATRIX a;
+
+
+void print_vector(vector<vector<double> > v){
     int n = v.size();
     for (int i = 0; i < n; i++){
         for (int j = 0; j< n; j++){
@@ -18,42 +24,64 @@ void print_vector(vector<vector<int> > v){
 }
 
 
-void * diag( void * p){
-    vector<vector<int> > v = *(reinterpret_cast< vector<vector<int> > *> (p) );
-    int n = v.size();
-    for (int i = 0; i<n;i++){
-        v[i][i]= 1;
+void* generateRandom(void* vn){
+    int n = (int) vn;
+    for (int i = 0; i < n/NUM_THREADS; i++){
+        ROW r;
+        for(int j = 0; j<n;j++){
+            r.push_back(1 + (drand48() * 100) );
+        }
+        a.push_back(r);
     }
-    cout<<"In DIAG: "<<endl;
-    print_vector(v);
-    p = (void*)&v;
-    return p;
+
 }
 
 
+// void * diag( void * p){
+//     vector<vector<int> > v = *(reinterpret_cast< vector<vector<int> > *> (p) );
+//     int n = v.size();
+//     for (int i = 0; i<n;i++){
+//         v[i][i]= 1;
+//     }
+//     cout<<"In DIAG: "<<endl;
+//     print_vector(v);
+//     p = (void*)&v;
+//     return p;
+// }
 
-void *ludecompose( vector<vector<int> >v,int n){
 
-    vector<vector<int> > l(n,vector<int>(n,0)),u(n,vector<int>(n,0));
 
-    pthread_t threads[NUM_THREADS];
-    void* dummy;
-    for (int i = 0; i < NUM_THREADS; i++){
-        int ti = pthread_create(&threads[i],NULL, &diag, &l);
-   }
-    int a = pthread_join(threads[0],&dummy);
-    // pthread_exit
-    cout<<a<<endl;
-    print_vector(l);
-    // int pthread_create();
-}
+// void *ludecompose( vector<vector<int> >v,int n){
+
+//     vector<vector<int> > l(n,vector<int>(n,0)),u(n,vector<int>(n,0));
+
+//     pthread_t threads[NUM_THREADS];
+//     void* dummy;
+//     for (int i = 0; i < NUM_THREADS; i++){
+//         int ti = pthread_create(&threads[i],NULL, &diag, &l);
+//    }
+//     int a = pthread_join(threads[0],&dummy);
+//     // pthread_exit
+//     cout<<a<<endl;
+//     print_vector(l);
+//     // int pthread_create();
+// }
 
 
 
 int main(){
-    int n =3;
-    vector<vector<int> >k(n,vector<int>(n,0));
-    ludecompose(k,n);
+    int n = 10;
+    pthread_t threads[NUM_THREADS];
+    for ( int i = 0; i < NUM_THREADS; i++){
+        int ti = pthread_create(&threads[i],NULL,generateRandom,(void *)n);  
+        // generateRandom(n);
+    }
+
+    for(int i =0; i< NUM_THREADS;i++){
+        void * d;
+        int a = pthread_join(threads[i],&d);
+    }
+    print_vector(a);
     return 0;
 
 }
